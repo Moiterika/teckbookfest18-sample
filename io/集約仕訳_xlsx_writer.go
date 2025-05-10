2431e690cecc4cce297d8f440a71d8b06a08fbdc
saeki-hisafumi <saeki-hisafumi@moiterika.jp>
2025-05-10 08:06:25 +0000

    Saveメソッドのシート管理を改善し、余分な行の削除処理を追加

--- io/集約仕訳_xlsx_writer.go
+++ io/集約仕訳_xlsx_writer.go
@@ -21,29 +21,42 @@ func New集約仕訳XlsxWriter(ef *excelize.File) *集約仕訳XlsxWriter {
 const sheet集約仕訳一覧 = "集約仕訳一覧"
 
 // Save は集計仕訳一覧をxlsxに書き出します
-func (w *集約仕訳XlsxWriter) Save(data []*domain.Ent集計仕訳) error {
-	// 既にシートが存在する場合は削除
-	if _, err := w.ef.GetSheetIndex(sheet集約仕訳一覧); err == nil {
-		if err := w.ef.DeleteSheet(sheet集約仕訳一覧); err != nil {
+func (w *集約仕訳XlsxWriter) Save(集約仕訳一覧 []*domain.Ent集計仕訳) error {
+	// シートの有無チェックと既存行取得
+	var sheetIdx int
+	var existingRowsCount int
+	if idx, err := w.ef.GetSheetIndex(sheet集約仕訳一覧); err == nil {
+		sheetIdx = idx
+		existingRows, _ := w.ef.GetRows(sheet集約仕訳一覧)
+		existingRowsCount = len(existingRows)
+	} else {
+		var err error
+		sheetIdx, err = w.ef.NewSheet(sheet集約仕訳一覧)
+		if err != nil {
 			return err
 		}
-	}
-
-	// シートを作成
-	idx, err := w.ef.NewSheet(sheet集約仕訳一覧)
-	if err != nil {
-		return err
 	}
 	// ヘッダー行を書き込み
 	headers := []interface{}{"計上年月", "コストプール", "按分ルール1", "按分ルール2", "借方税区分", "借方税率", "合計金額"}
 	w.ef.SetSheetRow(sheet集約仕訳一覧, "A1", &headers)
 	// データ行を書き込み
-	for i, e := range data {
+	for i, e := range 集約仕訳一覧 {
 		row := []interface{}{e.Fld計上年月, e.Fldコストプール, e.Fld按分ルール1, e.Fld按分ルール2, e.Fld借方税区分, e.Fld借方税率.String(), e.Fld合計金額.String()}
 		cell := fmt.Sprintf("A%d", i+2)
 		w.ef.SetSheetRow(sheet集約仕訳一覧, cell, &row)
 	}
-	// 新規シートをアクティブに設定
-	w.ef.SetActiveSheet(idx)
+	// 余分な行を削除（データ数が減った場合）
+	if existingRowsCount > len(集約仕訳一覧)+1 {
+		// 余分な行を1行ずつ削除
+		targetRowNum := len(集約仕訳一覧) + 2
+		count := existingRowsCount - (len(集約仕訳一覧) + 1)
+		for i := 0; i < count; i++ {
+			if err := w.ef.RemoveRow(sheet集約仕訳一覧, targetRowNum); err != nil {
+				return err
+			}
+		}
+	}
+	// シートをアクティブに設定
+	w.ef.SetActiveSheet(sheetIdx)
 	return nil
 }
